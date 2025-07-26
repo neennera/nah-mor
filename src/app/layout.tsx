39748c8572a63dfce9593f8b151b7b22bb9df41a
neennera <neennera@gmail.com>
2025-07-27 01:31:51 +0700

    fix image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
         <body className={`${inter.className} w-full bg-[#77856b] flex items-center justify-center`}>
             <div className='w-full md:max-w-[390px] bg-white min-h-screen flex flex-col'>
-                <nav className="bg-black text-white p-4">
+                {/* <nav className="bg-black text-white p-4">
                 <div className="container mx-auto flex justify-between items-center">
                     <h1 className="text-xl font-bold">Nah-Mor</h1>
                     <div className="space-x-4">
@@ -28,8 +28,8 @@ export default function RootLayout({
                     <a href="/analysis" className="hover:text-blue-200">Analysis</a>
                     </div>
                 </div>
-                  </nav>
-                  <nav className='w-full bg-black flex items-center justify-center'>
+                  </nav> */}
+                  <nav className='w-full bg-white flex items-center justify-center'>
                       <a href='/analysis/upload/1'>
                           <Image src='/logo.png' alt='logo' width={400} height={400} className='w-16 h-16' />
                       </a>
