146950d0f659cc07b1a3d7a85e15d49fb5fde6b8
neennera <neennera@gmail.com>
2025-07-26 23:16:58 +0700

    fix: name

--- src/api/voluntier/route.ts
+++ src/api/voluntier/route.ts
@@ -1,23 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
-let volunteerName = "ไม่ระบุชื่อ" // Default volunteer name
+let name = "ไม่ระบุชื่อ" // Default volunteer name
 
 export async function GET() {
   try {
-    return NextResponse.json({ volunteerName })
+    return NextResponse.json({ name })
   } catch (err) {
-    return NextResponse.json({ volunteerName }, { status: 500 })
+    return NextResponse.json({ name }, { status: 500 })
   }
 }
 
 export async function POST(req: NextRequest) {
   try {
-    const { name } = await req.json()
-    if (name && typeof name === 'string' && name.trim().length > 0) {
-      volunteerName = name.trim()
+    const { volunteerName } = await req.json()
+    if (volunteerName && typeof volunteerName === 'string' && volunteerName.trim().length > 0) {
+      name = volunteerName.trim()
     }
-    return NextResponse.json({ volunteerName })
+    
+    return NextResponse.json({ volunteerName : name })
   } catch (err) {
-    return NextResponse.json({ volunteerName }, { status: 500 })
+    return NextResponse.json({ volunteerName : name}, { status: 500 })
   }
 }
--- src/app/analysis/upload/1/page.tsx
+++ src/app/analysis/upload/1/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Webcam from 'react-webcam';
 
